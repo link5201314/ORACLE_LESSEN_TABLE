881fafcd6882bd20970485b3ad9162dd02f03ea8
isaacliu <link_5201314@hotmail.com>
2024-02-23 17:21:34 +0800

    chore(季排程)：完成 "[迴圈開始]逐一處理瘦身table" 初步框架

--- move_is_old_data.sql
+++ move_is_old_data.sql
@@ -1,35 +1,56 @@
---注意DB Link @TRWP101_TC 需替換為對應的DB
-drop PROCEDURE TRW.USP_MOVE_IS_OLD_DATA;
-
-CREATE OR REPLACE PROCEDURE TRW.USP_MOVE_IS_OLD_DATA()
+create or replace PROCEDURE TRW.USP_MOVE_IS_OLD_DATA
 AS
-	CURSOR cur_sel_LESSEN_TABLE;
+    cur_sel_LESSEN_TABLE sys_refcursor ;
+    
+    v_count NUMBER;
 	v_new_job_id NUMBER;
+    
+    v_table_name VARCHAR2(50);
+    v_sys_owner VARCHAR2(10);
+    v_is_enable CHAR(1);
+    
+    sql_list TRW.LESSEN_TABLE@TRWP101_TC%ROWTYPE;
+    
 BEGIN
-
 	--1) 為此次job取得最新的job_id
 	SELECT TRW.LESSEN_TABLE_MOVE_JOB_ID_SQ.NEXTVAL INTO v_new_job_id FROM dual;
 	dbms_output.put_line('v_new_job_id: ' || v_new_job_id);
-	
-	
-	CREATE GLOBAL TEMPORARY TABLE temp_data
-	ON COMMIT PRESERVE ROWS
-	AS
-	select * from TRW.LESSEN_TABLE@TRWP101_TC;
-	
-	cur_sel_LESSEN_TABLE IS select * from temp_data;
-
-	FOR sql_list IN cur_sel_LESSEN_TABLE LOOP
+    
     BEGIN
+        EXECUTE IMMEDIATE 'DROP TABLE TRW.LESSEN_TABLE';
+    EXCEPTION
+        WHEN OTHERS THEN
+        -- suppresses "table or view does not exist" exception
+        IF SQLCODE = -942 THEN
+            NULL; 
+        END IF;
+    END;
+    
+    EXECUTE IMMEDIATE 'CREATE TABLE TRW.LESSEN_TABLE as Select * from TRW.LESSEN_TABLE@TRWP101_TC';
+    
+    open cur_sel_LESSEN_TABLE for Select * from TRW.LESSEN_TABLE@TRWP101_TC;
+
+	--[迴圈開始]逐一處理瘦身table
+    Loop
+        FETCH cur_sel_LESSEN_TABLE INTO sql_list;
+        EXIT WHEN cur_sel_LESSEN_TABLE%NOTFOUND;
+    
 		v_table_name := sql_list.PK_LESSEN_TABLE;
         v_is_enable := sql_list.ENABLE;
         v_sys_owner := sql_list.SYS_OWNER;
 		
 		DBMS_OUTPUT.PUT_LINE('Processing table_name=' || v_table_name || '; SYS_OWNER=' || v_sys_owner || '; IS_ENABLE=' || v_is_enable);
+		--2) 確認上一季資料已完整搬移再清空table_bakhis：
 		
 	END LOOP;
-	
-
-	--dbms_output.put_line('Total Rows: ' || l_cur%rowcount);--here you will get total row count
+    
+    dbms_output.put_line('Total Rows: ' || cur_sel_LESSEN_TABLE%rowcount);--here you will get total row count;
+    
+    CLOSE cur_sel_LESSEN_TABLE;
 END;
-/
+
+DROP PROCEDURE USP_MOVE_IS_OLD_DATA;
+DROP PUBLIC SYNONYM USP_MOVE_IS_OLD_DATA;
+
+CREATE PUBLIC SYNONYM USP_MOVE_IS_OLD_DATA FOR TRW.USP_MOVE_IS_OLD_DATA;
+
